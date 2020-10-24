ed086014b91fc68b8cb18b10d9620b588096b056
Max Kim <hwaseen@gmail.com>
2020-10-24 11:42:29 +0900

    feat:drawChart 메서드를 세분화된 메서드로 분리

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <LineChart/>
+    <LineChart :data-set="dataSet"/>
+    <button @click="modifyData">modify dataset in parent</button>
   </div>
 </template>
 
@@ -11,6 +12,29 @@ export default {
   name: 'App',
   components: {
     LineChart
+  },
+  data:() => ({
+    dataSet: [
+      {name: 'a', value: 10},
+      {name: 'b', value: 29},
+      {name: 'c', value: 32},
+      {name: 'd', value: 25},
+      {name: 'e', value: 23},
+      {name: 'f', value: 15}
+    ],
+  }),
+  methods:{
+    modifyData() {
+      this.dataSet = [
+        {name: 'a', value: 24},
+        {name: 'b', value: 29},
+        {name: 'c', value: 25},
+        {name: 'd', value: 23},
+        {name: 'e', value: 21},
+        {name: 'f', value: 51},
+        {name: 'g', value: 71},
+      ]
+    }
   }
 }
 </script>
--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <button @click="makeChart">chart</button>
-    <button @click="dataModify">data modify</button>
+    <button @click="drawChart">chart</button>
     <section class='line-chart' />
   </div>
 </template>
@@ -11,82 +10,111 @@ import * as d3 from "d3";
 
 export default {
   name:'LineChart',
-  data:() => ({
-    width: 500,
-    height: 500,
-    margin: { top:40, right:40, bottom:40, left:40 },
-    dataSet: [
-      {name: 'a', value: 10},
-      {name: 'b', value: 29},
-      {name: 'c', value: 32},
-      {name: 'd', value: 25},
-      {name: 'e', value: 23},
-      {name: 'f', value: 15}
-    ],
-  }),
-  methods: {
-    makeChart() {
-      this.drawChart()
+  props: {
+    dataSet: {
+      type: Array,
+      default:() => [],
     },
-    dataModify() {
-      this.dataSet = [
-        {name: 'a', value: 25},
-        {name: 'b', value: 28},
-        {name: 'c', value: 12},
-        {name: 'd', value: 25},
-        {name: 'e', value: 43},
-        {name: 'f', value: 55}
-      ]
-      this.repaintChart()
+    width:{
+      type:Number,
+      default:500
+    },
+    height: {
+      type:Number,
+      default:500
+    },
+    margin:{
+      type:Object,
+      default: () => ({
+        top: 40,
+        right: 40,
+        bottom: 40,
+        left: 40
+      })
+    }
+  },
+  watch:{
+    dataSet: {
+      deep:true,
+      handler() {
+        this.drawChart()
+      }
+    }
+  },
+  methods: {
+    getXaxisScale() {
+      const { width, margin, dataSet } = this
+
+      const getXScale = d3.scaleBand()
+        .domain(dataSet.map(d => d.name))
+        .range([margin.left, width - margin.right])
+        .padding(0.2);
+      return getXScale
+    },
+    getYaxisScale() {
+      const { height, margin, dataSet } = this
+
+      const getYScale = d3.scaleLinear()
+      .domain([0, d3.max(dataSet, d => d.value)]).nice()
+      .range([height - margin.bottom, margin.top]);
+      return getYScale
+    },
+    getXaxis() {
+       const { height, margin } = this
+       const x = this.getXaxisScale()
+
+       const xAxis = g => g
+        .attr('transform', `translate(0, ${height - margin.bottom})`)
+        .call(d3.axisBottom(x).tickSizeOuter(0));
+      return xAxis
+    },
+    getYaxis() {
+       const { margin } = this
+       const y = this.getYaxisScale()
+
+       const yAxis = g => g
+        .attr('transform', `translate(${margin.left}, 0)`)
+        .call(d3.axisLeft(y))
+        .call(g => g.select('.domain').remove())
+      return yAxis
     },
     drawChart() {
      //* 속성값 DI
-     const {width, height, margin, dataSet} = this
-
-     //* x 척도
-     const x = d3.scaleBand()
-      .domain(dataSet.map(d => d.name))
-      .range([margin.left, width - margin.right])
-      .padding(0.2);
-  
-    //* y 척도
-    //! Nice:
-    const y = d3.scaleLinear()
-      .domain([0, d3.max(dataSet, d => d.value)]).nice()
-      .range([height - margin.bottom, margin.top]);
-  
-    //* x축 만들기
-    const xAxis = g => g
-      .attr('transform', `translate(0, ${height - margin.bottom})`)
-      .call(d3.axisBottom(x)
-      .tickSizeOuter(0));
-  
-    //* y축 만들기
-    const yAxis = g => g
-      .attr('transform', `translate(${margin.left}, 0)`)
-      .call(d3.axisLeft(y))
-      .call(g => g.select('.domain').remove());
+     const { width,height, dataSet } = this
+     const x = this.getXaxisScale()
+     const y = this.getYaxisScale()
   
     //! svg 진입점
-    const svg = d3.select('section').append('svg').style('width', width).style('height', height);
+    const svg = d3.select('section')
+      .append('svg')
+      .style('width', width)
+      .style('height', height);
+
+    const rects = svg.append('g')
+      .attr('fill', 'green')
+      .selectAll('rect')
+      .data(dataSet)
     
     //! 축 붙이기
-    svg.append('g').call(xAxis);
-    svg.append('g').call(yAxis);
+    svg.append('g').call(this.getXaxis());
+    svg.append('g').call(this.getYaxis());
 
-    //! enter로 데이터 바인딩
-    svg.append('g')
-        .attr('fill', 'green')
-        .selectAll('rect')
-        .data(dataSet).enter().append('rect')
+    //! enter
+    rects.enter().append('rect')
         .attr('x', d => x(d.name))
         .attr('y', d => y(d.value))
         .attr('height', d => y(0) - y(d.value))
-        .attr('width', x.bandwidth());
+        .attr('width', x.bandwidth())
 
-    svg.node();
+    rects
+      .attr('x', d => x(d.name))
+      .attr('y', d => y(d.value))
+      .attr('height', d => y(0) - y(d.value))
+      .attr('width', x.bandwidth())
     },
+    // watch로 연결하거나 하면 좋을듯
     repaintChart() {
+      console.log(this.dataSet)
       d3.select('section svg').remove()
       this.drawChart()
     }
@@ -95,5 +123,7 @@ export default {
 </script>
 
 <style scoped>
-
+  section {
+    transition: 2s;
+  }
 </style>
